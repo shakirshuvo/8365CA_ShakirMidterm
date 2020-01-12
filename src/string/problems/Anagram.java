bc7847ba3b9925e346580c58e1790e280ba634de
shakirshuvo <shakirshuvo@hotmail.com>
2020-01-12 09:06:21 -0800

    first push

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,5 +1,7 @@
 package string.problems;
 
+import java.util.Arrays;
+
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -8,5 +10,29 @@ public class Anagram {
     public static void main(String[] args) {
         //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
         //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+
+        areAnagram("Cat", "ACT"); // true
+        areAnagram("MARY", "ARMY"); // true
+        areAnagram("Sum", "um"); //false
+    }
+
+    public static boolean areAnagram (String word1, String word2) { // word1: "bob", word2: "obb"
+        // if both words are the same length
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+        // process both words into chracter arrays
+        char [] word1array = word1.toLowerCase().toCharArray(); // ['b', 'o', 'b']
+        char [] word2array = word2.toLowerCase().toCharArray(); // ['o', 'b', 'b']
+
+        Arrays.sort(word1array); // ['b', 'b', 'o']
+        Arrays.sort(word2array); // ['b', 'b', 'o']
+
+        for (int i=0; i < word1array.length; i++)  {
+            if (word1array[i] != word2array[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -6,6 +6,38 @@ public class Palindrome {
           If a String is reversed and it remains unchanged, that is called Palindrome. For example, MOM,DAD,MADAM are
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
+        String word1 = "racecar";
+        String word2 = "Mom";
+        String word3 = "Hello";
+        String word4 = "abba";
 
+        System.out.println(isPalindrome(word1)); //true
+        System.out.println(isPalindrome(word2)); // true
+        System.out.println(isPalindrome(word3)); // false
+        System.out.println(isPalindrome(word4)); // true
+    }
+
+    public static boolean isPalindrome(String word) { // Mom
+        String actualWord = word.toLowerCase(); // mom
+        String reversedWord = "";
+        for (int i = actualWord.length() - 1; i >= 0; i--) {
+            reversedWord = reversedWord + actualWord.charAt(i);
+        }
+        return actualWord.equals(reversedWord);
+
+    }
+
+    public static boolean isPalindromeWith2Pointers(String word) {
+        String actualWord = word.toLowerCase();
+        int startingPointer = 0;
+        int endingPointer = endingPointer = word.length()-1;
+        while (startingPointer < endingPointer) {
+            if (word.charAt(startingPointer) !=word.charAt(endingPointer)) {
+                return false;
+            }
+            startingPointer++;
+            endingPointer--;
+        }
+        return true;
     }
 }
